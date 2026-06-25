d01ce2c7eeca44adabdef610f23b630f513167a4
ramadhandev <ramadhan7695@gmail.com>
2026-06-25 08:30:34 +0700

    update assets report

--- ITAM/Controllers/ReportController.cs
+++ ITAM/Controllers/ReportController.cs
@@ -1,7 +1,7 @@
-﻿using ITAM.Models;
-using ITAM.Services;
+﻿using ITAM.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using OfficeOpenXml;
 
 namespace ITAM.Controllers
 {
@@ -15,22 +15,98 @@ namespace ITAM.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> ByLocation(int? locationId)
+        public async Task<IActionResult> ByLocation(
+            int? locationId,
+            int? categoryId,
+            int page = 1)
         {
-            var locations = await _serviceReport.GetAllLocationsAsync();
+            int pageSize = 10;
 
-            ViewBag.Locations =
-                new SelectList(locations, "Id", "Name", locationId);
+            var locations =
+                await _serviceReport.GetAllLocationsAsync();
 
-            if (locationId.HasValue)
+            var categories =
+                await _serviceReport.GetAllCategoriesAsync();
+
+            ViewBag.Locations = new SelectList(
+                locations,
+                "Id",
+                "Name",
+                locationId);
+
+            ViewBag.Categories = new SelectList(
+                categories,
+                "Id",
+                "Name",
+                categoryId);
+
+            var allData =
+                await _serviceReport.GetAssetsByFilterAsync(
+                    locationId,
+                    categoryId);
+
+            var totalItems = allData.Count;
+
+            var data = allData
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            ViewBag.Page = page;
+            ViewBag.PageSize = pageSize;
+            ViewBag.TotalItems = totalItems;
+
+            ViewBag.CurrentLocation = locationId;
+            ViewBag.CurrentCategory = categoryId;
+
+            return View(data);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ExportExcel(
+      int? locationId,
+      int? categoryId)
+        {
+            var data = await _serviceReport.GetAssetsByFilterAsync(
+                locationId,
+                categoryId);
+
+            ExcelPackage.License.SetNonCommercialOrganization("ITAM");
+
+            using var package = new ExcelPackage();
+
+            var worksheet = package.Workbook.Worksheets.Add("Asset Report");
+
+            worksheet.Cells[1, 1].Value = "Asset Tag";
+            worksheet.Cells[1, 2].Value = "Asset Name";
+            worksheet.Cells[1, 3].Value = "Category";
+            worksheet.Cells[1, 4].Value = "Location";
+            worksheet.Cells[1, 5].Value = "Status";
+            worksheet.Cells[1, 6].Value = "Condition";
+
+            int row = 2;
+
+            foreach (var item in data)
             {
-                var data =
-                    await _serviceReport.GetAssetsByLocationIdAsync(locationId.Value);
+                worksheet.Cells[row, 1].Value = item.AssetTag;
+                worksheet.Cells[row, 2].Value = item.AssetName;
+                worksheet.Cells[row, 3].Value = item.Category?.Name;
+                worksheet.Cells[row, 4].Value = item.Location?.Name;
+                worksheet.Cells[row, 5].Value = item.Status;
+                worksheet.Cells[row, 6].Value = item.Condition;
 
-                return View(data);
+                row++;
             }
 
-            return View(new List<Asset>());
+            worksheet.Cells.AutoFitColumns();
+
+            var fileBytes = package.GetAsByteArray();
+
+            return File(
+                fileBytes,
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                $"AssetReport_{DateTime.Now:yyyyMMddHHmmss}.xlsx");
         }
     }
+
 }
--- ITAM/Services/ServiceReport.cs
+++ ITAM/Services/ServiceReport.cs
@@ -13,23 +13,71 @@ namespace ITAM.Services
             _context = context;
         }
 
-        // AMBIL SEMUA LOKASI
+        // =========================
+        // LOKASI
+        // =========================
         public async Task<List<Location>> GetAllLocationsAsync()
         {
             return await _context.Locations
-        .AsNoTracking()
-        .OrderBy(x => x.Name)
-        .ToListAsync();
+                .AsNoTracking()
+                .OrderBy(x => x.Name)
+                .ToListAsync();
         }
 
-        // AMBIL ASSET BERDASARKAN LOKASI
+        // =========================
+        // KATEGORI
+        // =========================
+        public async Task<List<Category>> GetAllCategoriesAsync()
+        {
+            return await _context.Categories
+                .AsNoTracking()
+                .OrderBy(x => x.Name)
+                .ToListAsync();
+        }
+
+        // =========================
+        // UNTUK INSPECTION CONTROLLER
+        // =========================
         public async Task<List<Asset>> GetAssetsByLocationIdAsync(int locationId)
         {
             return await _context.Assets
                 .AsNoTracking()
                 .Include(x => x.Category)
                 .Include(x => x.Location)
-                .Where(x => x.LocationId == locationId && !x.IsDeleted)
+                .Where(x =>
+                    x.LocationId == locationId &&
+                    !x.IsDeleted)
+                .ToListAsync();
+        }
+
+        // =========================
+        // UNTUK REPORT FILTER
+        // =========================
+        public async Task<List<Asset>> GetAssetsByFilterAsync(
+            int? locationId,
+            int? categoryId)
+        {
+            var query = _context.Assets
+                .AsNoTracking()
+                .Include(x => x.Category)
+                .Include(x => x.Location)
+                .Where(x => !x.IsDeleted)
+                .AsQueryable();
+
+            if (locationId.HasValue)
+            {
+                query = query.Where(x =>
+                    x.LocationId == locationId.Value);
+            }
+
+            if (categoryId.HasValue)
+            {
+                query = query.Where(x =>
+                    x.CategoryId == categoryId.Value);
+            }
+
+            return await query
+                .OrderBy(x => x.AssetName)
                 .ToListAsync();
         }
     }
